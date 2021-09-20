ce6f8529c9615509d895d41da37dc647e42d2f85
dev-fjk <fjk.develop@gmail.com>
2021-09-20 16:52:55 +0900

    セクション3 数値

---
+++ basic_grammer/section3_basic.py
@@ -0,0 +1,19 @@
+# 超基礎文法系
+print("Hello World!")
+
+# 割り算を実施してくれるメソッド divmod() 割った結果とあまりを返却してくれる
+print(divmod(10, 3))
+
+# 変数 変数名 = 代入値 型推論あり
+# javaのデータ型についても pythonだとメモリの番地参照となるようなので注意(基本再代入はしない方がよさげ)
+num = 100
+print(num)
+
+# 型確認 この場合は<class 'int'>  <class 'str'>
+print(type(num))
+print(type('str'))
+
+# 型変換 float -> int
+floatNum = 10.1
+changeInt = int(floatNum)
+print(changeInt)
