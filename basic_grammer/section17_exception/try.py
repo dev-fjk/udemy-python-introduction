3eb443f76a126f357344af9a429d9106d739533e
dev-fjk <fjk.develop@gmail.com>
2024-10-07 17:43:12 +0900

    非推奨ファイルの修正

--- basic_grammer/section17_exception/try.py
+++ basic_grammer/section17_exception/try.py
@@ -3,9 +3,9 @@ your_input = input('数字を入れてください>')
 try:
     number = int(your_input)
     print(10 / number)
-except ValueError:  # except:だけにすると全エラーをcatch可能(非推奨)
+except ValueError:  # except:だけにすると全エラーをcatch可能(非推奨) -> 現状elseでよさそう
     print('数字以外が入力されました')
 except ZeroDivisionError:
     print('0は入力しないでください')
-except:
+else:
     print('その他エラー')
